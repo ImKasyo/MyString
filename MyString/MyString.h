9eb2244a2548ffbe94932999ffe8c097b954a018
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2024-09-30 00:59:20 +0300

    Добавил различные методы класса которые работают со строками

--- MyString/MyString.h
+++ MyString/MyString.h
@@ -6,5 +6,13 @@ class MyString
 public:
 	MyString();
 	MyString(const char* str);
+	MyString(const char* userStr);
+	MyString(const MyString& obj);
+	void Input();
+	void Print();
+	MyString(const MyString& str);
+	int MyStrLen();
+	void MyStrCat(MyString& b);
+	~MyString();
 };
 
